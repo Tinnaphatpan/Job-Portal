1de2b0075669369cb82b724d3208040ecb00ab12
tinnaphatpan-svg <tinnaphatpan@gmail.com>
2026-01-03 10:30:00 +0700

    chore: initialize Spring Boot project with Maven

---
+++ backend/src/main/java/com/jobportal/api/JobPortalApplication.java
@@ -0,0 +1,15 @@
+package com.jobportal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class JobPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalApplication.class, args);
+    }
+}
